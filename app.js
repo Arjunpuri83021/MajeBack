b1202f799db36b5feb2f3225a4cc76a5be169a67
arjun puri <thevirus991@gmail.com>
2024-07-05 17:44:06 +0530

    fully updated

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const apiRouter = require('./router/router');
+require('dotenv').config(); // Load environment variables
 
-mongoose.connect('mongodb+srv://arjunpuri991:<password>@cluster0.m5zairw.mongodb.net/MajeHiMaje')
+const app = express();
+
+// MongoDB connection using Mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Database is connected');
   })
@@ -19,12 +22,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Use your API router
-app.use(express.static('./public/uploads'))
+// Serve static files from the uploads directory
+app.use(express.static('./public/uploads'));
 
+// Use your API router
 app.use(apiRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
