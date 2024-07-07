d40e38ee8b6a67da2017ad42ef3622519c520058
arjun puri <thevirus991@gmail.com>
2024-07-07 15:08:51 +0530

    asf

--- router/router.js
+++ router/router.js
@@ -23,7 +23,7 @@ let storage = multer.diskStorage({
 
 
   router.get('/',(req,res)=>{
-    res.send("hello arjun")
+    res.send("hello arjun puri goswami")
   })
 
 router.post("/postdata",cAPi.data)
